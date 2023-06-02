a673b1d5ee18fedac9a17caecbd743448871c68e
Benito Serrano Garces <Benito.Serrano@aiep.cl>
2023-06-02 11:18:30 -0400

    primera subida de codigo base

---
+++ Api.FurnitureStore.API/Controllers/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿using Api.FurnitureStore.Data;
+using Api.FurnitureStore.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.FurnitureStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ApiFurnitureStoreContext _context;
+
+        public ClientsController(ApiFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Client>> Get()
+        {
+
+        }
+    }
+}
